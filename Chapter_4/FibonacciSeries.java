16d7cb4e999f365302c2ed6937c1800e0b3ba224
Stephane Joly <102809026+Scarecrow1965@users.noreply.github.com>
2023-05-29 15:40:05 -0500

    Add files via upload

---
+++ Chapter_4/FibonacciSeries.java
@@ -0,0 +1,33 @@
+// Exercise 7.29
+/* The Fibonacci series
+0, 1, 1, 2, 3, 5, 8, 13, 21, …
+begins with the terms 0 and 1 and has the property that each succeeding term is the sum of the two preceding terms.
+1.	Write a method fibonacci(n) that calculates the nth Fibonacci number.
+Incorporate this method into an application that enables the user to enter the value of n.
+2.	Determine the largest Fibonacci number that can be displayed on your system.
+3.	Modify the application you wrote in part (a) to use double instead of int to calculate
+ and return Fibonacci numbers, and use this modified application to repeat part (b).
+
+https://en.wikipedia.org/wiki/Fibonacci_number */
+
+import java.util.Scanner;
+
+public class FibonacciSeries
+{
+
+	public static void main(String[] args)
+	{
+		// initializing variables
+	int n; // users input
+
+	} // end method main
+
+	public static void Fibonacci()
+	{
+	int f1;
+	int f2;
+	int fibonacci;
+	//formula should be fibonacci = f1 + f2
+	}
+
+} // end class FibonacciSeries
